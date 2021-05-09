c528af906feeb2c1b0b9ce7d77d80ae016f1c9f2
Tiago Alves <alvesjtiago@gmail.com>
2021-05-09 15:05:20 +0200

    Updates

---
+++ precache-manifest.434116e2804c7545a4f7a0812bbdc718.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "857abd2b3232cecc276cea19d55a0026",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3e3854333b4b85a15702",
+    "url": "./static/css/main.55ed7b0f.chunk.css"
+  },
+  {
+    "revision": "c68abbe7cd24173b8684",
+    "url": "./static/js/2.ca8daadc.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.ca8daadc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e3854333b4b85a15702",
+    "url": "./static/js/main.6bc5e7dc.chunk.js"
+  },
+  {
+    "revision": "07a5b3f73df631097417",
+    "url": "./static/js/runtime-main.e9741701.js"
+  }
+]);
